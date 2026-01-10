190ea20fe179bd4ff8403da1f270e8e299fdab80
LEGION <rahuladhk0@gmail.com>
2026-01-10 17:25:50 +0930

    initial program main

---
+++ Main.py
@@ -0,0 +1,16 @@
+import encryption
+import decryption
+
+choice=input("Enter 1 to Encrypt, 2 to Decrypt or 3 to exit: ")
+
+if choice=="1":
+    #encryption process
+
+elif choice=="2":
+    #decryption process
+
+elif choice=="3":
+    break;
+
+else:
+    print("Error! Please Enter valid number.")

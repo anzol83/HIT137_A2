8c7fb14238e6925c23e1d0db8f0d370643ef17a2
LEGION <rahuladhk0@gmail.com>
2026-01-11 19:11:49 +0930

    fixing main according to module

--- Question 1/Main.py
+++ Question 1/Main.py
@@ -1,22 +1,25 @@
-import encryption
-import decryption
+from encryption import encrypt
+from decryption import decrypt
 
-choice=input("Enter 1 to Encrypt, 2 to Decrypt or 3 to exit: ")
+while True:
+    while True:
+        choice=input("Enter 1 to Encrypt, 2 to Decrypt or 3 to exit: ")
+        if choice=="1" or choice=="2" or choice=="3":
+            break
+        else:
+            print("Please enter valid number")
 
-if choice=="1":
-    shift_1=int(input("Enter shift 1 key: "))
-    shift_2=int(input("Enter shift 2 key: "))
-    process1=encrypt(shift_1, shift_2)
-    process1.start_encrypt()
+    if choice=="1":
+        shift_1=int(input("Enter shift 1 key: "))
+        shift_2=int(input("Enter shift 2 key: "))
+        process1=encrypt(shift_1, shift_2)
+        process1.start_encrypt()
 
-elif choice=="2":
-    shift_1=int(input("Enter shift 1 key: "))
-    shift_2=int(input("Enter shift 2 key: "))
-    process2=decrypt(shift_1, shift_2)
-    process2.start_decrypt()
+    elif choice=="2":
+        shift_1=int(input("Enter shift 1 key: "))
+        shift_2=int(input("Enter shift 2 key: "))
+        process2=decrypt(shift_1, shift_2)
+        process2.start_decrypt()
 
-elif choice=="3":
-    break;
-
-else:
-    print("Error! Please Enter valid number.")
+    else:
+        break

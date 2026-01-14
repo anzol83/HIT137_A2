b04c499370ea48e23b2a9b1960fb803496afbb49
Angel Neupane <angelneupane83@gmail.com>
2026-01-14 09:43:37 +0930

    drawing simple polygon

--- Question3/question3.py
+++ Question3/question3.py
@@ -1,14 +1,17 @@
 import turtle
 
 def main():
-    # Ask user for line length
+    # Ask user for polygon details
+    sides = int(input("Enter number of sides: "))
     side_length = float(input("Enter side length: "))
 
     screen = turtle.Screen()
     t = turtle.Turtle()
 
-    # Draw line using user input
-    t.forward(side_length)
+    # Draw a regular polygon
+    for _ in range(sides):
+        t.forward(side_length)
+        t.left(360 / sides)
 
     turtle.done()
 

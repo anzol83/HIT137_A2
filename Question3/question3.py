aff4b36470de8daabe11921cb3a8e14cc9e31669
Angel Neupane <angelneupane83@gmail.com>
2026-01-14 09:44:22 +0930

    writing polygon logic

--- Question3/question3.py
+++ Question3/question3.py
@@ -1,18 +1,18 @@
 import turtle
 
-def main():
-    # Ask user for polygon details
-    sides = int(input("Enter number of sides: "))
-    side_length = float(input("Enter side length: "))
-
-    screen = turtle.Screen()
+def draw_polygon(sides, side_length):
+    # Draw polygon using loop
     t = turtle.Turtle()
-
-    # Draw a regular polygon
     for _ in range(sides):
         t.forward(side_length)
         t.left(360 / sides)
 
+def main():
+    sides = int(input("Enter number of sides: "))
+    side_length = float(input("Enter side length: "))
+
+    turtle.Screen()
+    draw_polygon(sides, side_length)
     turtle.done()
 
 if __name__ == "__main__":

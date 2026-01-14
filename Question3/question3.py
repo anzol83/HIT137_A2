344d22fc50db185fc41aa16a405f2002e30a97a4
Angel Neupane <angelneupane83@gmail.com>
2026-01-14 09:39:47 +0930

    Initial commit

---
+++ Question3/question3.py
@@ -0,0 +1,12 @@
+# Import turtle module
+import turtle
+
+# Create screen and turtle
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+# Draw a simple line
+t.forward(100)
+
+# Keep window open
+turtle.done()

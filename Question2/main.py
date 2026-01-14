8ea8213716ac51a760eeff64a9985968dc536578
iDevilGamer <erankhattri@gmail.com>
2026-01-14 16:58:23 +0930

    Function for temp range

--- Question2/main.py
+++ Question2/main.py
@@ -35,7 +35,7 @@ def calculate_seasonal_averages(df):
     #Creating dictionary to store average temperatures for each season
     season_averages = {}
 
-    for season, months in SEASONS.items(): #loopinf through seasons and months
+    for season, months in SEASONS.items(): #looping through seasons and months
         values = df[months].values.flatten() #select the month column and convert the Numpyarray to 1-d array
         values = values[~np.isnan(values)] #Removes any missing values
         season_averages[season] = values.mean() #Calculate the average
@@ -44,3 +44,36 @@ def calculate_seasonal_averages(df):
     with open("average_temp.txt", "w") as f:
         for season, avg in season_averages.items():
             f.write(f"{season}: {avg:.1f}°C\n") #Write season name and average temperature to 1 decimal place
+
+#Function for calculating temperature range
+def calculate_temperature_ranges(df):
+    station_ranges = [] #Creating dictionary to store temperature range for each station
+
+    month_columns = list(
+        SEASONS["Summer"] + SEASONS["Autumn"] + SEASONS["Winter"] + SEASONS["Spring"]
+        ) #Combinig seasonal months into 1 column to go through whole year
+
+    for station, group in df.groupby("STATION_NAME"): #looping through the dataframe grouped by station name 'station'
+        values = group[month_columns].values.flatten() #Extract temperature values
+        values = values[~np.isnan(values)] #Removes any missing values
+
+        if len(values) == 0: #Checking if no valid temperature values exists
+            continue
+
+        max_temp = values.max()
+        min_temp = values.min()
+        temp_range = max_temp - min_temp #Calculating temperature range
+
+        #Storing the station name, temp ramge and max, min temp
+        station_ranges.append((station, temp_range, max_temp, min_temp))
+
+    max_range = max(sr[1] for sr in station_ranges) #Largest temperature range
+
+    with open("largest_temp_range_station.txt", "w") as f: #Create a file in write mode
+        for station, r, max_t, min_t in station_ranges: #loop through stored station ranges
+            if np.isclose(r, max_range): #checking if station range matches the largest range
+                #Now store the station name, temp range, max temp and min temp
+                f.write(
+                    f"{station}: Range {r:.1f}°C "
+                    f"(Max: {max_t:.1f}°C, Min: {min_t:.1f}°C)\n"
+                )
